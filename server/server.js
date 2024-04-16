bbf7996fa068bad045a72d7e3266e4af9dbadb18
pahopu <20520278@gm.uit.edu.vn>
2024-04-16 22:47:18 +0700

    Process when press search button

--- server/server.js
+++ server/server.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function showPosition(position) {
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
+}
+
+app.use(cors());
+
+app.get('/:dynamic', (req, res) => {
+    let { dynamic } = req.params;
+    if (dynamic == "currentLocation") {
+    }
+    else url = `http://api.weatherapi.com/v1/forecast.json?key=4e44dfbe320f4cb6b1725617241604&q=${dynamic}&days=5&aqi=no&alerts=no`;
+    fetch(url)
+        .then(res => { return res.json() })
+        .then(data => res.json(data))
+        .catch(error => res.json(error))
 })
 
 app.listen(port, () => {
--- web/resources/js/weather.js
+++ web/resources/js/weather.js
@@ -2,6 +2,65 @@ const searchBtn = document.getElementById("search-btn");
 const currLocationBtn = document.getElementById("my-location-btn");
 const input = document.getElementById("city-name");
 
-const location = document.getElementById("location");
+const locate = document.getElementById("locate");
+const dates = document.querySelectorAll(".date");
+const temps = document.querySelectorAll(".temp");
+const winds = document.querySelectorAll(".wind");
+const humids = document.querySelectorAll(".humid");
+const wtext = document.getElementById("wtext")
+const icons = document.querySelectorAll(".icon")
 
-const baseURL = 'http://api.weatherapi.com/v1/current.json?key=4e44dfbe320f4cb6b1725617241604&q=' + input.value;
+baseURL = 'http://localhost:3000/'
+
+searchBtn.addEventListener("click", getInfo);
+
+function getData(data) {
+    locate.innerText = data["location"]["name"];
+    dates.forEach((date, i) => {
+        date.innerText = data["forecast"]["forecastday"][i]["date"];
+    })
+    temps.forEach((temp, i) => {
+        temp.innerText = data["forecast"]["forecastday"][i]["day"]["avgtemp_c"];
+    })
+    winds.forEach((wind, i) => {
+        wind.innerText = (data["forecast"]["forecastday"][i]["day"]["maxwind_kph"] / 3.6).toFixed(2);
+    })
+    humids.forEach((humid, i) => {
+        humid.innerText = data["forecast"]["forecastday"][i]["day"]["avghumidity"];
+    })
+    wtext.innerText = data["forecast"]["forecastday"][0]["day"]["condition"]["text"];
+    icons.forEach((icon, i) => {
+        icon.setAttribute("src", data["forecast"]["forecastday"][i]["day"]["condition"]["icon"])
+    })
+}
+
+async function getInfo(e) {
+    e.preventDefault();
+    const res = await fetch(baseURL + input.value);
+    const data = await res.json();
+    getData(data);
+}
+
+coords = {
+    'lat': undefined,
+    'long': undefined
+}
+getLocation();
+
+currLocationBtn.addEventListener("click", getCurr);
+
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function showPosition(position) {
+    coords.lat = position.coords.latitude;
+    coords.long = position.coords.longitude;
+}
+
+async function getCurr(e) {
+    e.preventDefault();
+    getLocation();
+    const res = await fetch(baseURL + `currentLocation?lat=${coords.lat}&long=${coords.long}`);
+    const data = await res.json();
+}
